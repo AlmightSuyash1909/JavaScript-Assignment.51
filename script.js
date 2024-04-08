4591ada42d71290df14a6647352325e19073d59e
AlmightSuyash1909 <suyashprime1909@gmail.com>
2024-04-08 23:11:38 +0530

    writing test

---
+++ script.js
@@ -0,0 +1,38 @@
+// 1. Get full name 
+// Write a function that takes two input firstName and lastName  and returns full name. You will get the full name by adding first and last name with an space.
+// After writing the function write two tests for above function
+// Make the first test fail and look at the output
+// After making the first test fail do you see the output of the second test?
+
+   //  function getFullName (firstName,lastName) {
+   //      return (`${firstName} ${lastName}`)
+   //  }
+   //  console.log(getFullName("oggy", "Babu")) 
+   //  let result = getFullName("Oggy", "Master")
+   //  let expected = "Oggy Master"
+   //  if (result !== expected) {
+   //      throw new Error (`${result} is not equal to ${expected}`)
+   //  }
+   //  if (result == expected ) {
+   //      throw new Error (` ${result} is equal to ${expected}`) 
+   //  }
+
+
+// 2. Calculate total amount
+// Write a function that takes two input amount and taxRate and returns the total amount by amount + (amount * taxRate) 
+// Write two tests for the above function
+// Make the first test fail and look at the output
+// After making the first test fail do you see the output of the second test
+
+   //  function calculateTotalAmount (amount,taxrate) {
+   //      return amount + (amount*taxrate)
+   //  } 
+
+   //  let result = calculateTotalAmount(20,20)
+   //  let expected = 420
+   //  if (result !== expected ) {
+   //      throw new Error(`${result} is not equal to ${expected}`)
+   //  }
+   //  if ( result == expected ) {
+   //      throw new Error (`${result} is equal to ${expected} `)
+   //  }
